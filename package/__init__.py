6b4b42b6fc6cd3e947d5d39e61c9fac636e82df5
Cooper <Cooperransom08@gmail.com>
2023-02-22 14:53:38 -0500

    setup

---
+++ package/__init__.py
@@ -0,0 +1,5 @@
+# https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
+from modules import extractList
+from modules import extractTable
+from modules import extractdata
+
